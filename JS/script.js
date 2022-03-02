e3b428999cc0e13fae592f212647d7b8b9951a0f
uday-g-ithub15 <mudayhasan@gmail.com>
2022-03-02 13:57:39 +0600

    Try for sensors

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,58 @@
+/*
+ Get data from input field
+*/
 const getMobile = () => {
     const searchText = document.getElementById('search-bar').value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    //Some error if user dosen't write anything
     if (searchText.length == 0) {
         document.getElementById('blank-error').style.display = 'block'
+        document.getElementById('type-error').style.display = 'none';
         return getMobile;
-    }
-    else if (searchText.length > 0) {
-        document.getElementById('blank-error').style.display = 'none'
     }
     document.getElementById('search-bar').value = '';
     loaderShow('block');
     fetch(url)
         .then(res => res.json())
-        // .then(data => displayMobile(data.data))
         .then(data => {
             if (data.status) {
                 displayMobile(data.data)
+                // console.log(data)
             }
             else {
-                document.getElementById('type-error').style.display = 'block'
-                document.getElementById('blank-error').style.display = 'none'
-                document.getElementById('display-mobile').innerHTML = ''
+                document.getElementById('type-error').style.display = 'block';
+                document.getElementById('blank-error').style.display = 'none';
+                document.getElementById('load-more-btn').style.display = 'none';
+                document.getElementById('display-mobile').innerHTML = '';
                 loaderShow('none');
             }
         })
 
 }
-// getMobile();
-
+/* 
+    Display mobile after searching
+*/
 const displayMobile = (mobiles) => {
-    document.getElementById('type-error').style.display = 'none'
-    document.getElementById('blank-error').style.display = 'none'
+    document.getElementById('type-error').style.display = 'none';
+    document.getElementById('blank-error').style.display = 'none';
+    const first20 = mobiles.slice(0, 20);
+    if (first20.length >= 20) {
+        document.getElementById('load-more-btn').style.display = 'block';
+    }
+    const last = mobiles.splice(20, mobiles.length);
+    showMobileOnUi(first20);
+    document.getElementById('load-more-btn').addEventListener('click', function () {
+        showMobileOnUi(last);
+        document.getElementById('load-more-btn').style.display = 'none'
+    })
+    loaderShow('none');
+}
+// Common style for showing in ul
+const showMobileOnUi = (displayMobile) => {
     const showMobile = document.getElementById('display-mobile');
-    // showMobile.style.display = 'block'
     showMobile.classList.add('display-mobile');
     showMobile.textContent = '';
-    mobiles.forEach(mobile => {
-        // console.log(mobile);
+    displayMobile.forEach(mobile => {
         const mobileShow = document.createElement('div');
         mobileShow.classList.add('mobile-card');
         mobileShow.innerHTML = `
@@ -48,7 +64,6 @@ const displayMobile = (mobiles) => {
         showMobile.appendChild(mobileShow);
     }
     )
-    loaderShow('none');
 }
 
 //Get single mobile details
@@ -62,25 +77,37 @@ const getSingleMobile = (id) => {
 const displaySingleMobile = mobile => {
     const singleMobileDisplay = document.getElementById('single-mobile');
     singleMobileDisplay.textContent = '';
-    const mobileSensor = mobile.mainFeatures.sensors;
     const singleMobile = document.createElement('div');
-    singleMobile.classList.add('single-mobile')
+    singleMobile.classList.add('single-mobile');
+    mobile.mainFeatures.sensors.forEach(element => {
+        const h4 = document.createElement('h4');
+        h4.innerText = element;
+        // singleMobile.appendChild(h4);
+        singleMobileDisplay.appendChild(singleMobile.appendChild(h4))
+    })
     singleMobile.innerHTML = `
-    <div>
+    <div single-image>
     <img src = ${mobile.image}>
     </div>
-    <div>
+    <div class='single-text' id=''single-text>
     <h4>Name : ${mobile.name}</h4>
     <h4>Release Date : ${mobile.releaseDate ? mobile.releaseDate : 'No release date found'}</h4>
     <h4>Chipset : ${mobile.mainFeatures.chipSet}</h4>
     <h4>Display Size : ${mobile.mainFeatures.displaySize}</h4>
     <h4>Memory : ${mobile.mainFeatures.memory}</h4>
-    <h4>Sensors : ${mobile.mainFeatures.sensors}</h4>
+    <h4 id='sann'>Sensors : ${mobile.mainFeatures.sensors}</h4>
+    <h4>WLAN : ${mobile.others?.WLAN ? mobile.others.WLAN : 'No'}</h4>
+    <h4>Blutooth : ${mobile.others?.Bluetooth ? mobile.others.Bluetooth : 'No'}</h4>
+    <h4>GPS : ${mobile.others?.GPS ? mobile.others.GPS : 'No'}</h4>
+    <h4>NFC : ${mobile.others?.NFC ? mobile.others.NFC : 'No'}</h4>
+    <h4>Radio : ${mobile.others?.Radio ? mobile.others.Radio : 'No'}</h4>
+    <h4>USB : ${mobile.others?.USB ? mobile.others.USB : 'No'}</h4>
     </div>
     `
     singleMobileDisplay.appendChild(singleMobile);
 }
 
+//Declare loader function
 const loaderShow = display => {
     document.getElementById('loader').style.display = display;
 }
